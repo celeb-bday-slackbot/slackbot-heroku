be87e59a15ece7b56719609d976709bca55dc5d2
jadestern <prelasia@gmail.com>
2018-08-05 11:14:50 +0900

    파일 변수로 변경

--- app.js
+++ app.js
@@ -7,25 +7,18 @@ var controller = Botkit.slackbot({ debug: false });
 if (!process.env.slack_token_path) {
 	console.log('Info: Specify slack_token_path in environment');
 	startRTM(process.env.slack_token_path);
-} else {
-	fs.readFile(process.env.slack_token_path, function (err, data) {
-		if (err) {
-			console.log('Error: Specify token in slack_token_path file');
-			process.exit(1);
-		}
-		data = String(data);
-		startRTM(data);
-	});
 }
 
 controller.hears(
-		['hello', 'hi'], ['direct_message', 'direct_mention', 'mention', 'ambient'],
+		['hello', 'hi'],
+		['direct_message', 'direct_mention', 'mention', 'ambient'],
 		function (bot, message) {
 
-			bot.reply(message, 'Meow. :smile_cat: 테스트 1');
+			bot.reply(message, 'Meow. :smile_cat: 테스트 2');
 		});
 
-function startRTM(token){
+
+function startRTM(token) {
 	token = token.replace(/\s/g, '');
 	controller
 			.spawn({ token })
