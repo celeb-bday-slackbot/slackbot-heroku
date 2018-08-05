a9a7039053770adbbd8f32bf6f4c9dc56cb82c67
jadestern <prelasia@gmail.com>
2018-08-05 11:08:01 +0900

    환경변수로 token 사용 가능하게 변경

--- app.js
+++ app.js
@@ -5,29 +5,33 @@ var controller = Botkit.slackbot({ debug: false });
 
 // START: Load Slack token from file.
 if (!process.env.slack_token_path) {
-	console.log('Error: Specify slack_token_path in environment');
-	process.exit(1);
+	console.log('Info: Specify slack_token_path in environment');
+	startRTM(process.env.slack_token_path);
+} else {
+	fs.readFile(process.env.slack_token_path, function (err, data) {
+		if (err) {
+			console.log('Error: Specify token in slack_token_path file');
+			process.exit(1);
+		}
+		data = String(data);
+		startRTM(data);
+	});
 }
 
-fs.readFile(process.env.slack_token_path, function (err, data) {
-	if (err) {
-		console.log('Error: Specify token in slack_token_path file');
-		process.exit(1);
-	}
-	data = String(data);
-	data = data.replace(/\s/g, '');
+controller.hears(
+		['hello', 'hi'], ['direct_message', 'direct_mention', 'mention', 'ambient'],
+		function (bot, message) {
+
+			bot.reply(message, 'Meow. :smile_cat: 테스트 1');
+		});
+
+function startRTM(token){
+	token = token.replace(/\s/g, '');
 	controller
-			.spawn({ token: data })
+			.spawn({ token })
 			.startRTM(function (err) {
 				if (err) {
 					throw new Error(err);
 				}
 			});
-});
-// END: Load Slack token from file.
-
-controller.hears(
-		['hello', 'hi'], ['direct_message', 'direct_mention', 'mention'],
-		function (bot, message) {
-			bot.reply(message, 'Meow. :smile_cat:');
-		});
+}
